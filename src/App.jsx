363c845de689d0f0108cbae218b45528f90430be
Abhishek kumar <abhi.arya97161@gmail.com>
2024-05-03 17:31:48 +0530

    toggle using context api with main.jsx wrapping

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
-import React, { useEffect, useState } from "react";
-import { ThemeProvider } from "./contexts/theme";
+import React, { useEffect, useContext } from "react";
+import { ThemeContext } from "./contexts/theme";
 import ThemeBtn from "./comp/ThemeBtn";
 import Card from "./comp/Card";
 
 const App = () => {
-  const [themeMode, setThemeMode] = useState("light");
-
-  const lightTheme = () => {
-    setThemeMode("light");
-  };
-
-  const darkTheme = () => {
-    setThemeMode("dark");
-  };
+  const { themeMode } = useContext(ThemeContext);
 
   useEffect(() => {
     document.querySelector("html").classList.remove("light", "dark");
@@ -20,21 +12,16 @@ const App = () => {
   }, [themeMode]);
 
   return (
-    <>
-      <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
-        <div className="flex flex-wrap min-h-screen items-center">
-          <div className="w-full">
-            <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-              <ThemeBtn/>
-            </div>
-
-            <div className="w-full max-w-sm mx-auto">
-              <Card/>
-            </div>
-          </div>
+    <div className="flex flex-wrap min-h-screen items-center">
+      <div className="w-full">
+        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+          <ThemeBtn />
         </div>
-      </ThemeProvider>
-    </>
+        <div className="w-full max-w-sm mx-auto">
+          <Card />
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,32 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
 import App from "./App.jsx";
 import "./index.css";
 import { ThemeProvider } from "./contexts/theme.js";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    {/* <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}> */}
-      <App />
-    {/* </ThemeProvider> */}
-  </React.StrictMode>
-);
+const Main = () => {
+  const [themeMode, setThemeMode] = useState("light");
+
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
+
+  return (
+    <React.StrictMode>
+      <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+        <App />
+      </ThemeProvider>
+    </React.StrictMode>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(<Main />);
